ace0764e8a6c5498c1552dcd195aa35e89ecebb5
Michael Torbert <michaeltorbert@users.noreply.github.com>
2017-01-20 15:36:22 -0500

    fix importing of taxonomies #98 part 2

--- classes/import-export.php
+++ classes/import-export.php
@@ -477,10 +477,11 @@ if ( ! class_exists( 'SM_Import_Export' ) ) {
 												}
 
 												// This is just a failsafe. It also gives us access to vars the WP API created rather than from the CSV
-												if ( ! is_wp_error( $term ) && ! is_wp_error( $term_id ) && $term = get_term( (int) $term_id, $taxonomy ) ) {
+												if ( ! is_wp_error( $term_id ) && ( $term = get_term( (int) $term_id, $taxonomy ) ) && ! is_wp_error( $term ) ) {
 														// Associate (last var appends term to rather than replaces existing terms)
 														wp_set_object_terms( $id, $term->name, $taxonomy, true );
 														unset( $term );
+
 												}
 											}
 										}
