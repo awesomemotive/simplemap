824f5824163b9cdc926ba18862ce650558ecca33
contactashish13 <contactashish13@gmail.com>
2017-03-22 23:47:24 +0530

    New searches do not close map bubbles previously opened #92

--- classes/simplemap.php
+++ classes/simplemap.php
@@ -917,6 +917,7 @@ if ( ! class_exists( 'Simple_Map' ) ) {
 			}
 
 			function searchLocations( is_search ) {
+            clearInfoWindows();
 			// Init searchData
 			var searchData = {};
 			searchData.taxes = {};
